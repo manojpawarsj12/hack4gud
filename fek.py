5d223f8b2aabb9e5c20402217375580facd366a9
Manoj Pawar SJ <46449199+manojpawarsj12@users.noreply.github.com>
2019-10-23 20:02:24 +0530

    remove unncessory codes

--- fek.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-data = {'datetime':["a","b","c","d"],'attendence':[0,0,0,0],
-'regno':[38110078,38110271,38110306,38110301,]
-}
-df=pd.DataFrame(data=data,index=['Bharat', 'Kriti', 'dikshita', 'manju']) 
-print(df.head())
-df.loc['Bharat','attendence']=1
-print(df.head())
